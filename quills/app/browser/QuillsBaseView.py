12714fbacfa9c117bffad6c6ad684013a566227c
tomster <tomster@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-09-09 12:21:30 +0000

    removed reference to no longer existing `quills_tool` -> `manage_comments` is again callable ;-)

--- quills/app/browser/QuillsBaseView.py
+++ quills/app/browser/QuillsBaseView.py
@@ -4,10 +4,6 @@ from Products.CMFCore.utils import getToolByName
 class QuillsBaseView(BrowserView):
     """ Common base class for all Quills view classes. 
     """
-    def __init__(self, context, request):
-        super(QuillsBaseView, self).__init__(context, request)
-        self.quills_tool = getToolByName(context, 'quills_tool')
-
     def test(self, value, trueVal, falseVal):
         """
             helper method, mainly for setting html attributes.
