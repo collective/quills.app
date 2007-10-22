bafa61b31d2342385a88eb2b0213dd1bb3fe2d90
tomster <tomster@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-10-22 19:16:58 +0000

    archive months are now i18n-ified names rather than just numbers

--- quills/app/portlets/archive.py
+++ quills/app/portlets/archive.py
@@ -6,6 +6,7 @@ from plone.memoize.compress import xhtml_compress
 from plone.portlets.interfaces import IPortletDataProvider
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 
 # Quills imports
@@ -35,10 +36,24 @@ class Assignment(base.Assignment):
 class Renderer(base.Renderer, BaseView):
 
     _template = ViewPageTemplateFile('archive.pt')
+    
+    def __init__(self, context, request, view, manager, data):
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+        self.data = data
+        self._translation_service = getToolByName(self.context, 'translation_service')
 
     #@ram.cache(render_cachekey)
     def render(self):
         return xhtml_compress(self._template())
+
+    def getMonthName(self, month):
+        """Returns the current month name as a Message."""
+        msgid   = self._translation_service.month_msgid(month)
+        english = self._translation_service.month_english(month)
+        return _(msgid, default=english)
 
     @property
     def available(self):
@@ -48,11 +63,11 @@ class Renderer(base.Renderer, BaseView):
     def title(self):
         return _(PORTLET_TITLE)
 
-    #@property
-    #def getSubArchives(self):
-    #    weblog_content = recurseToInterface(self.context.aq_inner,
-    #                                       (IWeblog, IWeblogEnhanced))
-    #    return weblog_content.getSubArchives()
+    @property
+    def getSubArchives(self):
+       weblog_content = recurseToInterface(self.context.aq_inner,
+                                          (IWeblog, IWeblogEnhanced))
+       return weblog_content.getSubArchives()
 
 class AddForm(base.AddForm):
     form_fields = form.Fields(IWeblogArchivePortlet)
