09853c3fcd9c58864f13646040e1a938713c8ed8
svincic <svincic@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-12-30 13:41:48 +0000

    there is no adapater from Topic to Weblog so we use the old Topic.getWeblog method for now so feed on topics work

--- quills/app/syndication.py
+++ quills/app/syndication.py
@@ -81,7 +81,7 @@ class TopicFeedSource(BaseFeedSource):
     implements(IFeedSource)
 
     def __init__(self, context):
-        self.context = IWeblog(context)
+        self.context = context.getWeblog()
 
     def getFeedEntries(self):
         """See IFeedSoure
