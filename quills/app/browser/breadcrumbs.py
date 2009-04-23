70bf99f7f1db315803845428649e6165b69c0e0c
jhackel <jhackel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-23 08:08:13 +0000

    Fix for Quills issue #179: QuillsEnabled breaks Static Text Portlet globally.
    This fix does two things. First, it makes Quills custom breadcrumbs code 
    only kick in when inside a Weblog. This is done by tagging the REQUEST as 
    soon as traversal passes a Weblog container. Second, the way the 
    breadcrumb code filters unwanted traversal object, like the view, portlet 
    containers etc, is change. Formerly it would do so by "black listing" 
    unwanted object. As it turned out now and then a yet unkown object showed 
    up and slipped through, possibly breaking the code (as with this issue). 
    Now the breadcrumb code will white list known Quills components, throwing 
    away all other. While this is still not yery nice, it should be more 
    robust.

--- quills/app/browser/breadcrumbs.py
+++ quills/app/browser/breadcrumbs.py
@@ -1,21 +1,20 @@
-from zope.interface import implements
-from zope.component import getMultiAdapter
 from Acquisition import aq_base
-
-from zope.publisher.interfaces.browser import IBrowserView
-from Products.Five import BrowserView
-
-from Products.CMFCore.interfaces import IDiscussionResponse
-
 from Products.CMFPlone.browser.interfaces import INavigationBreadcrumbs
-from Products.CMFPlone.browser.navigation import PhysicalNavigationBreadcrumbs
+from quills.core.interfaces import IBaseContent, IWeblogEntry, IWeblogEnhanced
+from zope.component import getMultiAdapter
+from zope.interface import implements
 
-from quills.core.interfaces import IWeblogEntry
 
-
-class ArchiveAwareBreadcrumbs(BrowserView):
+class ArchiveAwareBreadcrumbs(object):
+    """Workaround ATContentTypes' habit to use the inner acquisition chain
+    for building the breadcrumbs. This would make the archive path disapear.
+    """
 
     implements(INavigationBreadcrumbs)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
 
     def breadcrumbs(self):
         """This method should return a tuple of the form:
@@ -24,32 +23,27 @@ class ArchiveAwareBreadcrumbs(BrowserView):
                      'Title': title_value,
              },)
         """
-        context = self.context
-        # This is *horrible*.  It seems like Five munges this view class to
-        # subclass Acquisition.Explicit, which then gives us an acquisition
-        # chain looking like self.context -> self (and that's it). What we want
-        # is the proper aq_chain back up through the weblog archive hierarchy
-        # that we have traversed through. That correct chain is available from
-        # the request...
-        request = self.request
-        # However, sometimes context can be what we expect to be parent because
-        # we have a view as the last path segment. So...
-        skip = 0
-        # in-place editing may provide us with a view in front, skip that
-        if IBrowserView.providedBy(request['PARENTS'][skip]):
-            skip += 1
-        # we do not want ourselves either
-        if aq_base(request['PARENTS'][skip]) == aq_base(context):
-            skip += 1
-        container = request['PARENTS'][skip]
-        # Now, we have to be careful about whether we are viewing a discussion
-        # reply, as this gives us another strange looking acquisition chain.
-        if IDiscussionResponse.providedBy(container):
-            obj = container.parentsInThread()[-1]
-            container = obj.aq_parent
-        view = getMultiAdapter((container, request), name='breadcrumbs_view')
-        crumbs = tuple(view.breadcrumbs())
-        crumbs += ({'absolute_url': context.absolute_url(),
-                   'Title': IWeblogEntry(context).getTitle(),
+        base = getMultiAdapter((self.container(),
+                                self.request), name='breadcrumbs_view')
+        crumbs = tuple(base.breadcrumbs())
+        crumbs += ({'absolute_url': self.context.absolute_url(),
+                   'Title': IWeblogEntry(self.context).getTitle(),
                    },)
         return crumbs
+
+    def container(self):
+        """Return the container of the WeblogEntry the breadcrumbs lead to.
+        
+        The previous implemention tried to skip over unwanted objects in the
+        parent stack. Now and then something new turned up and broke the
+        breadcrumbs (e.g. issue #179). This implemention will look for
+        something we know. This is still a bad hack, but more reliable and
+        a bit easier to grasp than the former (so I hope).
+        """
+        for parent in self.request['PARENTS']:
+            if ((IBaseContent.providedBy(parent) or
+                  IWeblogEnhanced.providedBy(parent))
+                and aq_base(parent) != aq_base(self.context)):
+                return parent
+        # Just in case. We would probably better raise an exception here.
+        return None
