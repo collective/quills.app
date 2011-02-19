6f8b3308487e450788609cf0e91247f8ff59d775
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-19 10:07:13 +0000

    Fixed indentation.

--- quills/app/browser/defaultviews.py
+++ quills/app/browser/defaultviews.py
@@ -17,34 +17,34 @@ class DefaultView(object):
 	self.request = request
 
     def publishTraverse(self, request, name):
-	"""See zope.publisher.interfaces.IPublishTraverse
-	
-	   This is not really needed as browserDefault returns our
-	   context and not ourselves. 
-	"""
-	return self.context.publishTraverse(request, name)
+        """See zope.publisher.interfaces.IPublishTraverse
+        
+        This is not really needed as browserDefault returns our
+        context and not ourselves. 
+        """
+        return self.context.publishTraverse(request, name)
 
     def browserDefault(self, request):
-	"""See  zope.publisher.interfaces.IBrowserPublisher"""
-	return (self.context, (self.name,))
+        """See  zope.publisher.interfaces.IBrowserPublisher"""
+        return (self.context, (self.name,))
 
 class DefaultArchiveView(DefaultView):
     """Provide the name of the archive default view.
     """
     def __init__(self, context, request):
-	super(DefaultArchiveView, self).__init__("@@archive_view",
-	                                         context, request)
+        super(DefaultArchiveView, self).__init__("@@archive_view",
+                                                context, request)
 
 class DefaultTopicsView(DefaultView):
     """Provide the name of the topic listing's default view.
     """
     def __init__(self, context, request):
-	super(DefaultTopicsView, self).__init__("@@topic_listing",
-	                                         context, request)
+        super(DefaultTopicsView, self).__init__("@@topic_listing",
+                                                context, request)
 
 class DefaultAuthorsView(DefaultView):
     """Provide the name of the authors listing's default view.
     """
     def __init__(self, context, request):
-	super(DefaultAuthorsView, self).__init__("@@author_listing",
-	                                         context, request)
+        super(DefaultAuthorsView, self).__init__("@@author_listing",
+                                                context, request)
